b723bef5856f814cb9bc4e99d8f6d0bf66d98056
chasangchual <sangchual.cha@hotmail.com>
2026-04-08 11:28:13 -0400

    add dependency, Authentik

--- init-db.sh
+++ init-db.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
 
+# If AUTHENTIK_DB is not set, default to 'authentik' 
+# or exit with an error so you don't get the "" error.
+: "${AUTHENTIK_DB:=authentik}"
+
+# 1. Create the database if it doesn't exist
+# We use " " around the db name in the SELECT to handle special characters
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-    SELECT 'CREATE DATABASE airflow'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
+    SELECT 'CREATE DATABASE "$AUTHENTIK_DB"'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$AUTHENTIK_DB') \gexec
 EOSQL
 
+# 2. Install extension in the primary/default database
+# Note: Extensions usually require superuser privileges
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS vector;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "airflow" <<-EOSQL
+# 3. Install extension in the newly created Authentik database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$AUTHENTIK_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS vector;
 EOSQL
